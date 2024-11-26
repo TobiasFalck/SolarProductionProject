f6ada4ff5167dd0c2ccd599987e862a6b2cf5a2c
simo002m <simo002m@gmail.com>
2024-11-26 11:40:02 +0100

    Added stuff from old repository

--- src/main/java/org/example/solarproductionproject/HelloController.java
+++ src/main/java/org/example/solarproductionproject/HelloController.java
@@ -3,14 +3,17 @@ package org.example.solarproductionproject;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController
-{
+public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick()
-    {
+    protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    public void initialize() {
+
     }
 }
