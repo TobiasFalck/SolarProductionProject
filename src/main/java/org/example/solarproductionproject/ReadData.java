0ef84a6b7297a27035c203eba125c769d7e5df97
hund555 <allanholmiversen@gmail.com>
2024-11-27 08:45:54 +0100

    Adjusted ReadDate utility class

--- src/main/java/org/example/solarproductionproject/ReadData.java
+++ src/main/java/org/example/solarproductionproject/ReadData.java
@@ -8,24 +8,26 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ReadData {
-    static int id = 0;
-    static String date = "";
-    static String time = "";
-    static int siteID = 0;
-    static int totalEnergyProduction = 0;
-    static int online = 0;
 
-    static DateTimeFormatter formatting = DateTimeFormatter.ofPattern("yyyy-MM-dd"); // set formatting for date
-    static ArrayList<SolarData> dataset = new ArrayList<SolarData>();
+    public static ArrayList<SolarData> readFileData(String filepath) throws FileNotFoundException
+    {
+        int id = 0;
+        String date = "";
+        String time = "";
+        int siteID = 0;
+        int totalEnergyProduction = 0;
+        int online = 0;
 
-    public static ArrayList<SolarData> readFileData(String filepath) throws FileNotFoundException {
+        DateTimeFormatter formatting = DateTimeFormatter.ofPattern("yyyy-MM-dd"); // set formatting for date
+        ArrayList<SolarData> dataset = new ArrayList<SolarData>();
 
         File inputFile = new File(filepath);
         Scanner in = new Scanner(inputFile).useDelimiter("[\t|T]"); // split by tabs and T
 
-            in.nextLine(); // skip first line (tsv table header)
+        in.nextLine(); // skip first line (tsv table header)
 
-            while (in.hasNext()) {
+        while (in.hasNext())
+        {
             // read data from tsv file
             id = in.nextInt();
             date = in.next();
@@ -43,6 +45,6 @@ public class ReadData {
             // add SolarData objects with arguments to arraylist
             dataset.add(new SolarData(id, dateConverted, timeConverted, siteID, totalEnergyProduction, online));
         }
-            return dataset;
+        return dataset;
     }
 }
