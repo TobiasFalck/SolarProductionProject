0e2ef50d8b51ae4aa30335edce2111faa7181892
TobiasFalck <TobiasFalck13@gmail.com>
2024-11-28 13:11:28 +0100

    Ui update in Scenebuilder

--- src/main/java/org/example/solarproductionproject/SolarController.java
+++ src/main/java/org/example/solarproductionproject/SolarController.java
@@ -2,85 +2,81 @@ package org.example.solarproductionproject;
 
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
-import javafx.scene.chart.BarChart;
-import javafx.scene.chart.XYChart;
+import javafx.scene.Scene;
+import javafx.scene.chart.*;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 
+import java.awt.*;
 import java.io.FileNotFoundException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
-public class SolarController
-{
+public class SolarController {
+
     private ArrayList<SolarData> data;
 
-
-
+    //region
     @FXML
     private Label errorMessage;
-
     @FXML
-    private ChoiceBox<String> siteDDL; // DDL: Drop Down List
-
+    private ChoiceBox<String> siteDDL; // Drop Down List for Sites
     @FXML
-    private DatePicker dateDP; // DP: Date Picker
-
+    private DatePicker dateDP; // Date Picker
     @FXML
-    private ChoiceBox<String> diagramTypeDDL;
-
+    private ChoiceBox<String> diagramTypeDDL; // Drop Down List for Chart Types
     @FXML
-    private BarChart<String, Integer> productionBarChart;
+    private BarChart<String, Integer> productionBarChart; // Bar Chart for Data
+    @FXML
+    private PieChart productionPieChart; // Pie Chart for Data
+    @FXML
+    private LineChart<String, Integer> productionLineChart;
+    @FXML
+    private Button dayButton, monthButton;
+    //endregion
 
 
     // Initializes and reads the data file
     @FXML
     public void initialize()
     {
-        try
-        {
+        try {
             data = ReadData.readFileData("src/resources/solar-dataset.tsv");
 
-            for (SolarData solarData : data)
-            {
-                // finds all siteIDs that are not currently added to siteDDL
-                if (!siteDDL.getItems().contains(String.valueOf(solarData.getSiteID())))
-                {
-                    // convert int to String, since choicebox only takes Strings
+            // Populate SiteID Dropdown
+            for (SolarData solarData : data) {
+                if (!siteDDL.getItems().contains(String.valueOf(solarData.getSiteID()))) {
                     siteDDL.getItems().add(String.valueOf(solarData.getSiteID()));
                 }
             }
-        }
-        catch (FileNotFoundException e)
-        {
+
+            // Populate Chart Type Dropdown
+            diagramTypeDDL.setItems(FXCollections.observableArrayList("Bar Chart", "Pie Chart", "Line Chart"));
+            diagramTypeDDL.setValue("Bar Chart"); // Default selection
+        } catch (FileNotFoundException e) {
             errorMessage.setText("File not found");
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             errorMessage.setText(e.getMessage());
         }
     }
 
-    // The on action for the button
-    public void createChartClick()
-    {
+    // Button click event to create charts
+    public void createChartClick() {
         ArrayList<Integer> totalsWhs = new ArrayList<>();
         ArrayList<Integer> times = new ArrayList<>();
 
-        errorMessage.setText(""); // hide error message
+        errorMessage.setText(""); // Hide error message
 
+        // Get user selections
         LocalDate datePicked = dateDP.getValue();
-        int siteIDPicked = Integer.parseInt(siteDDL.getValue()); // get siteID from sites choicebox and convert it to int
+        int siteIDPicked = Integer.parseInt(siteDDL.getValue()); // Convert selected site to int
 
-        // runs through every index in ArrayList data
-        for (int i = 0; i < data.size(); i++)
-        {
-            // check if date and site picked (based on siteID) corresponding to what is in the dataset
-            if (siteIDPicked == data.get(i).getSiteID() && datePicked.equals(data.get(i).getDate()))
-            {
-                totalsWhs.add(data.get(i).getWattPerHour()); // add hourly total kWh to ArrayList totals
-                times.add(data.get(i).getTime());
+        // Filter data based on selected site and date
+        for (SolarData solarData : data) {
+            if (siteIDPicked == solarData.getSiteID() && datePicked.equals(solarData.getDate())) {
+                totalsWhs.add(solarData.getWattPerHour());
+                times.add(solarData.getTime());
             }
         }
 
@@ -90,30 +86,83 @@ public class SolarController
         }
         else
         {
-           createDayChart(siteIDPicked, datePicked, totalsWhs, times);
-        }
+            // Get selected chart type and create appropriate chart
+            String selectedChart = diagramTypeDDL.getValue();
 
+            // Hide all charts initially
+            productionBarChart.setVisible(false);
+            productionPieChart.setVisible(false);
+
+            // Display and update the selected chart - Removes active charts
+            switch (selectedChart) {
+                case "Bar Chart":
+                    createDayChart(siteIDPicked, datePicked, totalsWhs, times);
+                    productionBarChart.setVisible(true);
+                    productionPieChart.setVisible(false);
+                    productionLineChart.setVisible(false);
+                    break;
+                case "Pie Chart":
+                    createPieChart(siteIDPicked, datePicked, totalsWhs);
+                    productionPieChart.setVisible(true);
+                    productionLineChart.setVisible(false);
+                    productionBarChart.setVisible(false);
+                    break;
+                case "Line Chart":
+                    createLineChart(siteIDPicked, datePicked, totalsWhs, times);
+                    productionLineChart.setVisible(true);
+                    productionPieChart.setVisible(false);
+                    productionBarChart.setVisible(false);
+                    break;
+                default:
+                    errorMessage.setText("Invalid chart type selected");
+            }
+        }
     }
 
     /**
-     * Adds the given data into a bar chart
+     * Creates a Bar Chart based on provided data
+     */
+    public void createDayChart(int siteIDPicked, LocalDate datePicked, ArrayList<Integer> totalWhs, ArrayList<Integer> times) {
+        XYChart.Series<String, Integer> series = new XYChart.Series<>();
+        series.setName("Site ID: " + siteIDPicked + "\nDate: " + datePicked.toString());
+
+        for (int i = 0; i < totalWhs.size(); i++) {
+            series.getData().add(new XYChart.Data<>(times.get(i) + ":00", totalWhs.get(i)));
+        }
+
+        productionBarChart.setData(FXCollections.observableArrayList(series));
+    }
+
+    /**
+     * Creates a Pie Chart based on provided data
+     */
+    public void createPieChart(int siteIDPicked, LocalDate datePicked, ArrayList<Integer> totalWhs) {
+        productionPieChart.getData().clear();
+
+        for (int i = 0; i < totalWhs.size(); i++) {
+            productionPieChart.getData().add(new PieChart.Data("" + i, totalWhs.get(i)));
+        }
+
+        productionPieChart.setTitle("Site ID: " + siteIDPicked + "\nDate: " + datePicked.toString());
+    }
+
+    /**
+     * Adds the given data into a LineChart
      * @param siteIDPicked
      * @param datePicked
      * @param totalWhs
      * @param times
      */
-    public void createDayChart(int siteIDPicked, LocalDate datePicked, ArrayList<Integer> totalWhs, ArrayList<Integer> times)
-    {
-        XYChart.Series<String, Integer> series = new XYChart.Series();
+    public void createLineChart(int siteIDPicked, LocalDate datePicked, ArrayList<Integer> totalWhs, ArrayList<Integer> times) {
+        XYChart.Series<String, Integer> series = new XYChart.Series<>();
         series.setName("Site ID: " + siteIDPicked + "\nDate: " + datePicked.toString());
 
-        for (int i = 0; i < totalWhs.size(); i++)
-        {
-            series.getData().add(new XYChart.Data(times.get(i) + ":00", totalWhs.get(i)));
-            System.out.println(totalWhs.get(i));
+        for (int i = 0; i < totalWhs.size(); i++) {
+            series.getData().add(new XYChart.Data<>(times.get(i) + ":00", totalWhs.get(i)));
         }
 
-        // replace data in barchart instead of replacing it
-        productionBarChart.setData(FXCollections.observableArrayList(series));
+        // Clear old data and set new data
+        productionLineChart.getData().clear();
+        productionLineChart.getData().add(series);
     }
 }
