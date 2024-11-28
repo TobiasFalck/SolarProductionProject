775cd2a4de831b131a8c788a7c9e4a4ae2b926db
simo002m <simo002m@gmail.com>
2024-11-28 09:15:29 +0100

    Added createDayChart() method to SolarController

--- src/main/java/org/example/solarproductionproject/SolarController.java
+++ src/main/java/org/example/solarproductionproject/SolarController.java
@@ -17,7 +17,6 @@ import java.util.ResourceBundle;
 
 public class SolarController
 {
-    static boolean dataInBarChart = false;
     static ArrayList<SolarData> data;
 
     static {
@@ -27,11 +26,6 @@ public class SolarController
             throw new RuntimeException(e);
         }
     }
-
-    // create ArrayLists for future use
-    static ArrayList<Integer> totals = new ArrayList<>();
-    static ArrayList<Integer> times = new ArrayList<>();
-
 
     @FXML
     private Label errorMessage;
@@ -65,42 +59,50 @@ public class SolarController
         }
     }
 
-    public void createChartClick() {
-        totals.clear(); // clear out potentially "old" data from totals
+    public void createChartClick()
+    {
+        ArrayList<Integer> totals = new ArrayList<>();
+        ArrayList<Integer> times = new ArrayList<>();
+
         errorMessage.setText(""); // hide error message
 
         LocalDate datePicked = dateDP.getValue();
         int siteIDPicked = Integer.parseInt(siteDDL.getValue()); // get siteID from sites choicebox and convert it to int
 
         // run through every index in ArrayList data
-        for (int i = 0; i < data.size(); i++) {
-
+        for (int i = 0; i < data.size(); i++)
+        {
             // check if date and site picked(based on siteID) correspond to what is in the dataset
-            if (siteIDPicked == data.get(i).getSiteID() && datePicked.equals(data.get(i).getDate())) {
-                totals.add(data.get(i).getTotalEnergyProduction()); // add hourly total kWh to ArrayList totals
+            if (siteIDPicked == data.get(i).getSiteID() && datePicked.equals(data.get(i).getDate()))
+            {
+                totals.add(data.get(i).getWattPerHour()); // add hourly total kWh to ArrayList totals
                 times.add(data.get(i).getTime());
             }
         }
-        if (totals.isEmpty()) {
+
+        if (totals.isEmpty())
+        {
             errorMessage.setText("No data for\nchosen date");
         }
+        else
+        {
+           createDayChart(siteIDPicked, datePicked, totals, times);
+        }
 
-        // create bar chart
+    }
+
+    public void createDayChart(int siteIDPicked, LocalDate datePicked, ArrayList<Integer> totalWhs, ArrayList<Integer> times) {
         XYChart.Series<String, Integer> series = new XYChart.Series();
         series.setName("Site ID: " + siteIDPicked + "\nDate: " + datePicked.toString());
 
-        for (int i = 0; i < totals.size(); i++) {
-                series.getData().add(new XYChart.Data(times.get(i) + ":00", totals.get(i)));
-                System.out.println(totals.get(i));
+        for (int i = 0; i < totalWhs.size(); i++)
+        {
+            series.getData().add(new XYChart.Data(times.get(i) + ":00", totalWhs.get(i)));
+            System.out.println(totalWhs.get(i));
         }
 
-        // only run if date is valid/has data
-        if (!totals.isEmpty()) {
-            // replace data in barchart instead of replacing it
-            productionBarChart.setData(FXCollections.observableArrayList(series));
-        }
+        // replace data in barchart instead of replacing it
+        productionBarChart.setData(FXCollections.observableArrayList(series));
     }
-
-
 
 }
